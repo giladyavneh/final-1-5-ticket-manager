a1eaebf1b4c2220459627869187663054a1628bd
giladyavneh <67857522+giladyavneh@users.noreply.github.com>
2020-08-24 16:07:23 +0300

    posr tequests working

--- server/app.js
+++ server/app.js
@@ -1,16 +1,36 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
-const fs=require("fs")
+const fs = require("fs");
 
-app.use(express.json())
-const path=process.env.TICKETS_VARIABLE||"data.json"
+app.use(express.json());
 
-app.get("/api/tickets",(req,res)=>{
-    let tickets=JSON.parse(fs.readFileSync(path))
-    let searchWord=req.query.searchText;
-    let regex=new RegExp(searchWord,'i');
-    let outcome=tickets.filter(ticket=>regex.test(ticket.title))
-    res.send(outcome)
-})
+const path = process.env.TICKETS_VARIABLE || "data.json";
+const tickets = JSON.parse(fs.readFileSync(path));
+
+app.get("/api/tickets", (req, res) => {
+  let tickets = JSON.parse(fs.readFileSync(path));
+  let searchWord = req.query.searchText;
+  let regex = new RegExp(searchWord, 'i');
+  let outcome = tickets.filter(ticket => regex.test(ticket.title));
+  res.send(outcome);
+});
+
+app.post('/api/tickets/:ticketId/done', (req, res) => {
+  let id = req.params.ticketId;
+  let tickets = JSON.parse(fs.readFileSync(path));
+  let ticket = tickets.find(ticket => ticket.id === id);
+  ticket.done = true;
+  fs.writeFileSync("data.json", JSON.stringify(tickets));
+});
+
+app.post('/api/tickets/:ticketId/undone', (req, res) => {
+  let id = req.params.ticketId;
+  let tickets = JSON.parse(fs.readFileSync(path));
+  let ticket = tickets.find(ticket => ticket.id === id);
+  ticket.done = false;
+  fs.writeFileSync("data.json", JSON.stringify(tickets));
+});
+
 
 module.exports = app;
