7ee3a78c1381d2e7c0d0e716896ea8904d286090
giladyavneh <67857522+giladyavneh@users.noreply.github.com>
2020-08-24 15:10:49 +0300

    get method that returns all ticket objects with the searched word in the title
    case-insensetive

--- server/app.js
+++ server/app.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const fs=require("fs")
 
+app.use(express.json())
+const path=process.env.TICKETS_VARIABLE||"data.json"
+
+app.get("/api/tickets",(req,res)=>{
+    let tickets=JSON.parse(fs.readFileSync(path))
+    let searchWord=req.query.searchText;
+    let regex=new RegExp(searchWord,'i');
+    let outcome=tickets.filter(ticket=>regex.test(ticket.title))
+    res.send(outcome)
+})
 
 module.exports = app;
