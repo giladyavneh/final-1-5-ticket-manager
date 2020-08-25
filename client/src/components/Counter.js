7ab271d0a190b3f332a0918b007b164c32ce6739
giladyavneh <67857522+giladyavneh@users.noreply.github.com>
2020-08-25 18:51:24 +0300

    styled it

--- client/src/components/Counter.js
+++ client/src/components/Counter.js
@@ -1,11 +1,15 @@
 import React from "react";
-
+import "./counter.css"
 function Counter(props){
-    
-    return(
-        props.count>0?
-        <div>there are <span id="hideTicketsCounter">{props.count}</span> hidden tickets <button id="restoreHideTickets" onClick={props.restore}>restore</button></div>:
+    const hiddens=props.count>0?
+        <div>There {props.count===1?"is":"are"} <span id="hideTicketsCounter">{props.count}</span> hidden tickets 
+            <button id="restoreHideTickets" onClick={props.restore}>restore</button></div>:
         <div></div>
+    return(
+        <div className="counter">
+            
+            {hiddens}
+        </div>
     )
 }
 
